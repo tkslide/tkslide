06caf6f5bc4ed6736932d2e790de2580170c7505
Rafal M. Sulejman <rms@poczta.onet.pl>
2013-10-08 15:40:50 +0200

    changes for SNOBOL4 ver. 1.5

--- slide.bat
+++ slide.bat
@@ -1,4 +1,5 @@
 @echo off
 rem set TEMP=C:\temp
-set SNOLIB="%CD\snobol4-1.2\lib"
+set SNOLIB="%CD\snobol4-1.5\lib"
+set SNODIR="%CD\snobol4-1.5"
 "%CD%\runtime\tclkit.exe" "%CD%\tkslide.tcl" %1 %2 %3 %4 %5 %6 %7 %8 %9
